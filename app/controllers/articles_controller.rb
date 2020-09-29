bf9f896a7ac8122af9be79385d8a71e54d5d25a6
Diego Lira <diegoalira@gmail.com>
2020-09-29 07:55:44 -0300

    Form-base workflow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     
     def index
         @articles = Article.all
@@ -10,12 +11,31 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+
     def create
-        @article = Article.new(
-            title: params[:article][:title],
-            body: params[:article][:body])
-          @article.save
-          redirect_to article_path(@article)
-      end
+        @article = Article.new(article_params)
+        @article.save
+        flash.notice = "Article '#{@article.title}' Created!"
+        redirect_to article_path(@article)
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "Article '#{@article.title}' Updated!"
+      
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id]) 
+        @article.destroy
+        flash.notice = "Article '#{@article.title}' Deleted!"
+        redirect_to articles_path
+    end        
 
 end
